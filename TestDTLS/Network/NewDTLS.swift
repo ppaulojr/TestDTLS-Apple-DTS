6f227c811e22b4d72daf827de3f7d8615a8d4a62
ppaulojr <ppaulojr@gmail.com>
2019-01-18 11:09:22 -0200

    Using newtwork framework

---
+++ TestDTLS/Network/NewDTLS.swift
@@ -0,0 +1,38 @@
+//
+//  NewDTLS.swift
+//  TestDTLS
+//
+//  Created by Pedro Paulo Oliveira Junior on 17/01/19.
+//  Copyright © 2019 Trinity. All rights reserved.
+//
+
+import Foundation
+import Network
+
+class DTLSiOSNew {
+    var endpoint : NWEndpoint?
+    var connection: NWConnection?
+    let myQueue = DispatchQueue(label: "cellQueue")
+
+    
+    init(gateway: Endpoint) {
+        let param = NWParameters.dtls
+
+        param.prohibitedInterfaceTypes = [.cellular]
+        connection = NWConnection(host: NWEndpoint.Host(gateway.address), port: 80, using: param)
+        connection?.stateUpdateHandler = { (newState) in
+            switch(newState) {
+            case .ready:
+            print("Connected")
+            case .waiting(let error):
+                self.connection?.restart()
+                print("Waiting: \(error)")
+            case .failed(let error):
+                print("Failed: \(error)")
+            default:
+                break
+            }
+        }
+        connection?.start(queue: myQueue)
+    }
+}
--- TestDTLS/ViewController.swift
+++ TestDTLS/ViewController.swift
@@ -14,13 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var ipField: UITextField!
     @IBOutlet weak var ifaceSelector: UISegmentedControl!
     @IBAction func connect(_ sender: Any) {
-        let cell = CellularUDPSession(gateway: Endpoint(address: ipField.text ?? "", port: "80", version: .four))
-        useCell = (ifaceSelector.selectedSegmentIndex == 0)
-        if cell.doSSLHandshake(useCell) == 0 {
-            presentAlert("Works")
-        } else {
-            presentAlert("Does not work")
-        }
+        let dtls = DTLSiOSNew(gateway: Endpoint(address: ipField.text ?? "", port: "80", version: .four))
+        
+//        let cell = CellularUDPSession(gateway: Endpoint(address: ipField.text ?? "", port: "80", version: .four))
+//        useCell = (ifaceSelector.selectedSegmentIndex == 0)
+//        if cell.doSSLHandshake(useCell) == 0 {
+//            presentAlert("Works")
+//        } else {
+//            presentAlert("Does not work")
+//        }
     }
     
     func presentAlert(_ message: String!) {
